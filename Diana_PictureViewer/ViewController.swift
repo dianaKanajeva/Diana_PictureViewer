1abb276826427213ba958ba1daf661568067bdb2
dianaKanajeva <dkmurze@gmail.com>
2016-05-07 14:57:05 +0300

    Update ViewController.swift

--- Diana_PictureViewer/ViewController.swift
+++ Diana_PictureViewer/ViewController.swift
@@ -10,19 +10,18 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-   // @IBOutlet weak var displayImageView: NSImageView!
-
     @IBOutlet weak var displayImage: NSImageView!
     @IBOutlet weak var pathTextField: NSTextField!
     @IBOutlet weak var displayImageView: NSImageView!
     @IBOutlet weak var displayScrollView: NSScrollView!
-    
     @IBOutlet weak var imageCell: NSImageCell!
+    
     private var tempPath: String!
     private var imgOriginalWidth: CGFloat!
     private var imgOriginalHeight: CGFloat!
     private var x: CGFloat!
     private var y: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         displayImageView.imageScaling = .ScaleAxesIndependently
@@ -48,9 +47,12 @@ class ViewController: NSViewController {
 
     @IBAction func magnificationRecognizer(sender: AnyObject) {
         if tempPath != nil {
+             // Check that image is not too small (size at least 100 x 100)
             if ((displayImageView.frame.width * (sender.magnification + 1.0 )) > 100 ) &&  ((displayImageView.frame.height * (sender.magnification + 1.0)) > 100){
+                // Expand an image
                 displayImageView.frame = CGRect(x: 0, y: 0, width: displayImageView.frame.width * (sender.magnification + 1.0 ), height: displayImageView.frame.height * (sender.magnification + 1.0))
-                    displayScrollView.documentView?.setFrameSize(CGSize(width: displayImageView.frame.width, height: displayImageView.frame.height))
+                // Change displayScrollView size
+                displayScrollView.documentView?.setFrameSize(CGSize(width: displayImageView.frame.width, height: displayImageView.frame.height))
             }
         }
     }
