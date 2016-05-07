63f130feeafcbd827ec92b42c8d58f7616a9920b
dianaKanajeva <dkmurze@gmail.com>
2016-05-07 14:22:33 +0300

    Added files via upload

---
+++ Diana_PictureViewerTests/Diana_PictureViewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Diana_PictureViewerTests.swift
+//  Diana_PictureViewerTests
+//
+//  Created by Diana Kanajeva in May of 2016.
+//  Copyright © 2016 Diana Kanajeva. All rights reserved.
+//
+
+import XCTest
+@testable import Diana_PictureViewer
+
+class Diana_PictureViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
